a6a709c60934beb0f21067c005448f2d2972aabc
DK\mnas <mnas@sitecore.net>
2017-03-23 12:45:01 +0900

    commit patch

---
+++ src/Sitecore.Support.153571/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.153571")]
+[assembly: AssemblyProduct("Sitecore.Support.153571")]
+[assembly: ComVisible(false)]
